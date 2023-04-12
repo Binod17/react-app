560a6ab05b228794ffb38ac8bd2bd7432a3baa6b
Binod Shah Thakuri <129031432+Binod17@users.noreply.github.com>
2023-04-12 23:41:12 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - My name is Bibek Shah
+          Learn React - click here to learn more !!
         </a>
       </header>
     </div>
